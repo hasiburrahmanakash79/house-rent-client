b1079fe0a8444a8757714cfcbf90023c1de5cccf
saifularafat <saifularafat.info@gmail.com>
2023-08-05 21:33:24 +0600

    Banner section, Button, section taitle and Container section is create by Components folder

--- src/Pages/Booking/Booking.jsx
+++ src/Pages/Booking/Booking.jsx
@@ -1,19 +1,7 @@
-import Banner from "../../components/Banner";
-import Button from "../../components/Button";
-import Container from "../../components/Container";
-import SectionTitle from "../../components/SectionTitle";
-
 const Booking = () => {
     return (
         <div className="">
-            <Banner title='Booking' subTitle='Booking now your house' />
-            <Container>
-                <h1>Booking page</h1>
-                <SectionTitle
-                    title='Booking Section'
-                    description='WELCOME TO OUR HOUSE RENT COMPANY' />
-                <Button adders='/blogs' label='All Apartments' />
-            </Container>
+            
         </div>
     );
 };
