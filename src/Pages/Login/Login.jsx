d0352f1199373517e8f61521e99c395fcdb3b856
Abir-07 <md.tazwarul.islam.07@gmail.com>
2023-08-06 21:58:41 +0600

    add login input filed simple validation

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import loginLogo from '../../assets/login.png'
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
   //code verification
@@ -16,7 +17,17 @@ const Login = () => {
     console.log(event.target.value)
     setisButtonEnable(event.target.value === randomNumbers.join(''));
   }
- //verification end
+  //verification end
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
+
+  const onSubmit = (data) => console.log(data)
+
   return (
     <div>
       <div className="hero min-h-screen bg-base-200">
@@ -29,31 +40,34 @@ const Login = () => {
           </div>
           <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
             <div className="card-body">
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                  </label>
+                  <input {...register("email", { required: true })} type="email" placeholder="email" className="input input-bordered" />
+                  {errors.email && <span>Email field is required</span>}
+                </div>
 
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input type="text" placeholder="email" className="input input-bordered" />
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Password</span>
+                  </label>
+                  <input {...register("password", { required: true })} type="password" placeholder="password" className="input input-bordered" />
+                  {errors.password && <span>Password field is required</span>}
+                  <label className="label">
+                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                  </label>
+                </div>
+                <div className="form-control ">
+                  <input disabled={!isButtonEnable} className="btn btn-primary " type='submit' value={'Login'} />
+                </div>
+              </form>
+              <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+                <p className='text-center bg-green-500 text-white w-1/3 md:w-full px-2 py-1 rounded-lg mx-auto'> {randomNumbers.join(' ')}</p>
+                {/* varification input field */}
+                <input onChange={handleInputChange} type="text" placeholder="Type Number" className="input input-sm w-[117px] md:w-full mx-auto input-bordered" />
               </div>
-
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input type="text" placeholder="password" className="input input-bordered" />
-                <label className="label">
-                  <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                </label>
-              </div>
-              <div className="form-control ">
-                <input  disabled={!isButtonEnable} className="btn btn-primary " type='submit' value={'Login'} />
-              </div>
-            <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
-            <p className='text-center bg-green-500 text-white w-1/3 md:w-full px-2 py-1 rounded-lg mx-auto'> {randomNumbers.join(' ')}</p>
-              {/* varification input field */}
-              <input  onChange={handleInputChange} type="text" placeholder="Type Number" className="input input-sm w-[117px] md:w-full mx-auto input-bordered" />
-            </div>
               <label className="label">
                 <p className="label-text-alt ">Don't Have Any Account? <Link className='link link-hoverF' to='/signup'>Click Here</Link></p>
               </label>
--- src/Pages/Signup/Signup.jsx
+++ src/Pages/Signup/Signup.jsx
@@ -62,7 +62,7 @@ const Signup = () => {
                   <button className="btn btn-primary">Signup Now</button>
                 </div>
                 <label className="label">
-                  <p className="label-text-alt ">Already Have Any Account? <Link className='link link-hoverF' to='/signup'>Click Here</Link></p>
+                  <p className="label-text-alt ">Already Have Any Account? <Link className='link link-hoverF' to='/login'>Click Here</Link></p>
                 </label>
               </form>
             </div>
