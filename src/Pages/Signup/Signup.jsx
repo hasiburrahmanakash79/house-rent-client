ae8c4e4813922c385f864ac65dfc5d1bd87f52c8
Abir-07 <md.tazwarul.islam.07@gmail.com>
2023-08-06 19:17:21 +0600

    impliment react hook form in signup page

--- src/Pages/Signup/Signup.jsx
+++ src/Pages/Signup/Signup.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import loginLogo from '../../assets/login.png'
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 const Signup = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
+
+  const onSubmit = (data) => console.log(data)
+
   return (
     <div>
       <div className="hero min-h-screen bg-base-200">
@@ -14,39 +24,45 @@ const Signup = () => {
           </div>
           <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
             <div className="card-body">
-              <div className="form-control">
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Name:</span>
+                  </label>
+                  <input {...register("name", { required: true })} type="text" placeholder="Name" className="input input-bordered" />
+                  {errors.name && <span>Name field is required</span>}
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                  </label>
+                  <input {...register("email", { required: true })} type="email" placeholder="email" className="input input-bordered" />
+                  {errors.email && <span>Email field is required</span>}
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Address</span>
+                  </label>
+                  <input {...register("address", { required: true })} type="text" placeholder="Address" className="input input-bordered" />
+                  {errors.email && <span>Address field is required</span>}
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Password</span>
+                  </label>
+                  <input {...register("password", { required: true })} type="text" placeholder="password" className="input input-bordered" />
+                  {errors.password && <span>Password field is required</span>}
+                  <label className="label">
+                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                  </label>
+                </div>
+                <div className="form-control mt-6">
+                  <button className="btn btn-primary">Signup Now</button>
+                </div>
                 <label className="label">
-                  <span className="label-text">Name:</span>
+                  <p className="label-text-alt ">Already Have Any Account? <Link className='link link-hoverF' to='/signup'>Click Here</Link></p>
                 </label>
-                <input type="text" placeholder="Name" className="input input-bordered" />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input type="text" placeholder="email" className="input input-bordered" />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Address</span>
-                </label>
-                <input type="text" placeholder="Address" className="input input-bordered" />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input type="text" placeholder="password" className="input input-bordered" />
-                <label className="label">
-                  <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                </label>
-              </div>
-              <div className="form-control mt-6">
-                <button className="btn btn-primary">Signup Now</button>
-              </div>
-              <label className="label">
-                  <p  className="label-text-alt ">Already Have Any Account? <Link className='link link-hoverF' to='/signup'>Click Here</Link></p>
-                </label>
+              </form>
             </div>
           </div>
         </div>
