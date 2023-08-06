10dbcec9cd85c3d609356296789dba279116632b
bijoymamud <bijoymamud.09@gmail.com>
2023-08-06 23:48:01 +0600

    button

--- src/Pages/HomePage/Apartments.jsx
+++ src/Pages/HomePage/Apartments.jsx
@@ -14,22 +14,23 @@ const Apartments = () => {
       <h1>Rooms & Apartments</h1>
 
       <div className="grid lg:grid-cols-3 ">
-      {apartments.map((apartment) => 
-        <div key={apartment.id}  >
-          <div className="card w-96 bg-base-100 shadow-xl">
-            <figure>
-              <img src={apartment?.image} />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">{apartment?.apartment_type}</h2>
-              <p>{apartment?.location}</p>
-              <div className="card-actions justify-between">
-                <h2>{apartment?.price}</h2>
+        {apartments.map((apartment) =>
+          <div key={apartment.id}  >
+            <div className="card w-96 bg-base-100 shadow-xl">
+              <figure>
+                <img src={apartment?.image} />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">{apartment?.apartment_type}</h2>
+                <p>{apartment?.location}</p>
+                <div className="card-actions justify-between">
+                  <h2>{apartment?.price}</h2>
+                  <button>Hello</button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      )}
+        )}
       </div>
     </div>
   );
