3bf965c2f17ba0d630e03a85032e5dde878eeb1f
Abir-07 <md.tazwarul.islam.07@gmail.com>
2023-08-06 21:51:11 +0600

    add code verification for enable login button

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import loginLogo from '../../assets/login.png'
 import { Link } from 'react-router-dom';
 
 const Login = () => {
-  const dark = localStorage.getItem('darkMode');
+  //code verification
+  const [randomNumbers, setRandomNumbers] = useState([])
+  const [isButtonEnable, setisButtonEnable] = useState(false)
+
+  useEffect(() => {
+    const newRandomNumbers = Array.from({ length: 6 }, () => Math.floor(Math.random() * 10));
+    setRandomNumbers(newRandomNumbers);
+  }, [])
+
+  const handleInputChange = (event) => {
+    console.log(event.target.value)
+    setisButtonEnable(event.target.value === randomNumbers.join(''));
+  }
+ //verification end
   return (
     <div>
       <div className="hero min-h-screen bg-base-200">
@@ -16,29 +29,34 @@ const Login = () => {
           </div>
           <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
             <div className="card-body">
- 
+
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
                 <input type="text" placeholder="email" className="input input-bordered" />
               </div>
-       
+
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Password</span>
                 </label>
-                <input tFype="text" placeholder="password" className="input input-bordered" />
+                <input type="text" placeholder="password" className="input input-bordered" />
                 <label className="label">
                   <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                 </label>
               </div>
-              <div className="form-control mt-6">
-                <button className="btn btn-primary">Logins</button>
+              <div className="form-control ">
+                <input  disabled={!isButtonEnable} className="btn btn-primary " type='submit' value={'Login'} />
               </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+            <p className='text-center bg-green-500 text-white w-1/3 md:w-full px-2 py-1 rounded-lg mx-auto'> {randomNumbers.join(' ')}</p>
+              {/* varification input field */}
+              <input  onChange={handleInputChange} type="text" placeholder="Type Number" className="input input-sm w-[117px] md:w-full mx-auto input-bordered" />
+            </div>
               <label className="label">
-                  <p  className="label-text-alt ">Don't Have Any Account? <Link className='link link-hoverF' to='/signup'>Click Here</Link></p>
-                </label>
+                <p className="label-text-alt ">Don't Have Any Account? <Link className='link link-hoverF' to='/signup'>Click Here</Link></p>
+              </label>
             </div>
           </div>
         </div>
